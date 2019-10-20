143dd1de3091f13a4fb9ff549a9c55dcb30c5ccb
mcxim <gelfandmaxim@gmail.com>
2019-10-20 12:01:58 +0300

    Add test suite, add first version of initBoard

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,10 @@
-module Lib
-  ( someFunc
-  )
-where
+module Lib where
 
-import           System.Random                  ( randomRIO )
+import qualified System.Random                 as R
+import           Control.Monad                  ( (>=>) )
+import qualified Data.Set                      as S
+import qualified Data.List                     as L
+import           Test.Hspec
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
@@ -15,10 +16,10 @@ data Difficulty = Difficulty {height :: Int, width :: Int, numMines :: Int}
   deriving Show
 
 beginner = Difficulty { height = 9, width = 9, numMines = 10 }
-intermediate = Difficulty { height = 16, width = 16, numMines = 0 }
-expert = Difficulty { height = 16, width = 30, numMines = 0 }
+intermediate = Difficulty { height = 16, width = 16, numMines = 40 }
+expert = Difficulty { height = 16, width = 30, numMines = 99 }
 
-data Cell = Cell Int State
+data Cell = Cell {number :: Int, state :: State}
   deriving Show
 
 type Board = [[Cell]]
@@ -26,9 +27,42 @@ type Board = [[Cell]]
 inRange :: (Num a, Ord a) => (a, a) -> a -> Bool
 inRange (lower, upper) num = num <= upper && num >= lower
 
+changeCell :: Board -> Cell -> (Int, Int) -> Board
+changeCell board cell (row, col) =
+  take row board
+    ++ [take col (board !! row) ++ [cell] ++ drop (col + 1) (board !! row)]
+    ++ drop (row + 1) board
+
 emptyBoard :: Difficulty -> Board
 emptyBoard difficulty =
   replicate (height difficulty) $ replicate (width difficulty) (Cell 9 Closed)
 
+composeMN :: (Monad m) => Int -> (a -> m a) -> a -> m a
+composeMN 1 f = f
+composeMN n f = f >=> composeMN (n - 1) f
+
+addRandomMine :: Board -> IO Board
+addRandomMine board = undefined
+
 initBoard :: Difficulty -> IO Board
-initBoard = undefined
+initBoard difficulty =
+  let board = emptyBoard difficulty
+      matrix =
+          [ [ (row, col) | col <- [0 .. width difficulty] ]
+          | row <- [0 .. height difficulty]
+          ]
+      idxs = concat matrix
+      genMineIdxs =
+          (\g -> map (idxs !!) $ take
+              (numMines difficulty)
+              (L.nub $ R.randomRs (0, length idxs - 1) g)
+            )
+            <$> R.newStdGen
+  in  do
+        mineIdxs <- genMineIdxs
+        return
+          . (map . map)
+              (\idx ->
+                if idx `elem` mineIdxs then Cell 0 Closed else Cell 9 Closed
+              )
+          $ matrix
